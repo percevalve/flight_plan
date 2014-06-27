733ec400f3d0886d6f6fb186db551daeb84119a4
Severin Hatt <hatt_s@mit.edu>
2014-06-27 21:49:14 +0200

    change secret for produciton

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-FlightPlan::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+FlightPlan::Application.config.secret_key_base = '5358efde9d390a03512d0f8e3c3ffb6e81caeaaab210aff878c094688e1376960c8ef5237ab5b4105154bd24b6a5038c937e54fc2d75e7404e9ad19cc32101d8'
