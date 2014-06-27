86b875bb215abc2ebb2e97968a8dcabd7c6275f0
Severin Hatt <hatt_s@mit.edu>
2014-06-27 21:44:08 +0200

    change secret for produciton

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,5 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-secret = Rails.env.production? ? ENV['SECRET_TOKEN'] : '58511c3a92eec8ea2ca67b95e1699230bfc0ace8b7f5a70d29cf9bb58eec76709f56f9c042fe678f31322f6c391c52af54cbfd8723e00bbdb0da56046943e6f2'
-FlightPlan::Application.config.secret_key_base = secret
+FlightPlan::Application.config.secret_key_base = ENV['SECRET_TOKEN']
